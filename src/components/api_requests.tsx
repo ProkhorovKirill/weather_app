b92df57e9a5a8833a6b4b3b319ed5d36c6468b28
ProkhorovKirill <Prokhorov187@yandex.ru>
2025-05-12 14:34:43 +0300

    .

--- src/components/api_requests.tsx
+++ src/components/api_requests.tsx
@@ -1,11 +1,11 @@
 export async function getCoordinates(city: string, api_key: string){
-    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city},0&limit=5&appid=${api_key}`);
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},0&limit=5&appid=${api_key}`);
     const coords = await response.json();
     return coords
 }
 
 export async function getWeather(lat: number, lon: number, api_key: string){
-    const responseWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`);
+    const responseWeather = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`);
     const weatherData = await responseWeather.json();
     return weatherData
 }
